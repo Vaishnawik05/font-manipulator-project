96e96eb7de2e8d7e8f7fd56780dccd302001b2e4
Vaishnawik05 <79893634+Vaishnawik05@users.noreply.github.com>
2022-02-01 16:28:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
+leftWristX= 0;
+rightWristX= 0;
+difference= 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
 
     canvas = createCanvas(550, 550);
-    canvas.position(560, 160)
+    canvas.position(560, 180)
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
 
 function draw(){
-    background('');
+    background('#006400');
+    document.getElementById("font_size").innerHTML = difference +"px";
+    textSize(difference);
+    fill('#90EE90');
+    text('Vaishnawi', 275, 275);
 }
 
 function modelLoaded(){
@@ -22,5 +30,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
     }
 }
